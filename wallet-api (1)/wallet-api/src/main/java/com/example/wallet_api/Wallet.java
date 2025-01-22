789ce933284c85d02069f9159c61da32edbadb72
Ivaylo Tsekov <ivo172004@gmail.com>
2025-01-22 21:53:26 +0200

    walletAppDemo

---
+++ wallet-api (1)/wallet-api/src/main/java/com/example/wallet_api/Wallet.java
@@ -0,0 +1,42 @@
+package com.example.wallet_api;
+
+// those imports allow me to use the stuff starting with
+// @ which on the other hand is the interaction of the database
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity  // Marks the class as an entity for JPA
+public class Wallet {
+
+    @Id  // Marks this field as the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Automatically generates a unique ID
+    private Long id;
+
+    private double balance;
+
+    public Wallet() {
+        this.balance = 0.0;
+    }
+
+    public Wallet(double balance) {
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ wallet-api (1)/wallet-api/src/main/java/com/example/wallet_api/repository/WalletRepository.java
@@ -0,0 +1,6 @@
+package com.example.wallet_api.repository;
+
+import com.example.wallet_api.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+}
